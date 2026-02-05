cd1d6e47065b8d0d4479a35a1f02acdafe7a6079
Vicente Cutri <cutristk@gmail.com>
2026-02-05 13:05:32 -0300

    service layer

---
+++ src/main/java/com/webServises/microEmpresa/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.webServises.microEmpresa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webServises.microEmpresa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
---
+++ src/main/java/com/webServises/microEmpresa/services/UserService.java
@@ -0,0 +1,27 @@
+package com.webServises.microEmpresa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webServises.microEmpresa.entities.User;
+import com.webServises.microEmpresa.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
